9e64e5cbf8ceccbd86cb1d292b312874306c1100
Ivenousername <anondaun42@gmail.com>
2025-11-11 13:42:33 +0700

    Completed

--- OOP_1/OOP_1.cpp
+++ OOP_1/OOP_1.cpp
@@ -5,18 +5,20 @@ int main()
 	CStrArray arr(10);
 
 	arr += new CStr();
+	cout << arr;
 	arr += new CStr("Hello World");
+	*arr[0] = *arr[1];
 	arr += new CStr("Allo World");
 	arr += new CStr();
 	arr += new CStr();
+	arr += new CStr();
+	arr += new CStr(*arr[3]);
+	cout << *arr[0];
 	cout << "The array: " << arr << endl;
 	arr.sort_len();
 	cout << "Array sorted by length: " << arr << endl;
-	cout << "Sorted by content: " << static_cast<bool>(arr.check_sort()) << endl;
+	cout << "Sorted by content: " << arr.check_sort() << endl;
 	arr.sort_content();
 	cout << "Array sorted by content : " << arr << endl;
-	cout << "Sorted by content: " << static_cast<bool>(arr.check_sort()) << endl;
-	//В функции main нужно создать объект CStrArray с массивом заданной длины, 
-	//	создать и добавить в массив несколько объектов CStr, провести сортировку по содержимому(с проверкой) 
-	//	и длине строк и вывести отсортированные массивы.
+	cout << "Sorted by content: " << arr.check_sort() << endl;
 }
--- OOP_1/СStr.h
+++ OOP_1/СStr.h
@@ -4,7 +4,7 @@
 #include <cstring>ee
 #include <cstdlib>
 #include <random>
-#include <chrono>
+#include <chrono> 
 using namespace std;
 class CStr
 {
